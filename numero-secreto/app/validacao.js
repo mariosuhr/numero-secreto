68810d92949a33509cd51a1b103761998ae476da
mariosuhr <61993274+mariosuhr@users.noreply.github.com>
2023-03-28 18:47:46 -0300

    Add files via upload

---
+++ numero-secreto/app/validacao.js
@@ -0,0 +1,47 @@
+function verificaValido(chute){
+    const numero = parseInt(chute)        
+
+    if(chuteInvalido(numero)){
+        elementoChute.innerHTML += '<div>Valor inválido</div>'
+        return
+    }
+
+    if(numeroMaiorOuMenor(numero)){
+        elementoChute.innerHTML += `<div>Valor inválido: o número secreto precisa estar entre 
+        ${menorValor} e ${maiorValor} </div>
+        `
+
+        return
+    }
+
+    if(numero === numeroSecreto){
+        document.body.innerHTML = `
+        <h2>Você acertou!</h2>
+        <h3>O número secreto era ${numeroSecreto}.</h3>
+
+        <button id="jogar-novamente" class="btn-jogar"> Jogar Novamente </button>
+        `
+    } else if(numero > numeroSecreto){
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-solid fa-arrow-down"></i></div>
+        `
+    } else {
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-solid fa-arrow-up"></i></div>
+        `
+    }
+}
+
+function chuteInvalido(numero) {
+    return Number.isNaN(numero)
+}
+
+function numeroMaiorOuMenor(numero){
+    return numero > maiorValor || numero < menorValor
+}
+
+document.body.addEventListener('click', e => {
+    if(e.target.id == 'jogar-novamente'){
+        window.location.reload()
+    }
+})
